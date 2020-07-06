49625ad02f7b0e2fc21397cf1ffa28d8246609bb
yy53176 <yy53176@126.com>
2020-07-06 15:21:55 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Summer</div>
+                <div class="title">Hello Laravel! - by QDB</div>
             </div>
         </div>
     </body>
